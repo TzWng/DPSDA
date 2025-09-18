4c3827c59f4d45366a21a5a241c6e4d7ccc10b6b
TzWng <91708166+TzWng@users.noreply.github.com>
2025-09-18 06:12:33 -0400

    Update pe.py

--- pe/runner/pe.py
+++ pe/runner/pe.py
@@ -273,7 +273,8 @@ class PE(object):
                     execution_logger.info(f"noise for mu GDP: {sub_sigma}")
 
             # Generate or load initial data.
-            syn_data = Yelp(root_dir="/content/drive/MyDrive/SecPE/train/augpe_infty")
+            # syn_data = Yelp(root_dir="/content/drive/MyDrive/SecPE/yelp/yelp_augpe_infty")
+            syn_data = OpenReview(root_dir="/content/drive/MyDrive/SecPE/train/augpe_2p")
             syn_data.data_frame.reset_index(drop=True, inplace=True)
             syn_data.metadata.iteration = 0
             syn_data.data_frame["PE.VARIATION_API_FOLD_ID"] = -1
