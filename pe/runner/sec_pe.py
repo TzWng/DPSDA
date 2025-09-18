34b6520d4bd96e3c0bc17ce04150d350524bc44c
TzWng <91708166+TzWng@users.noreply.github.com>
2025-09-18 04:19:38 -0400

    Update sec_pe.py

--- pe/runner/sec_pe.py
+++ pe/runner/sec_pe.py
@@ -236,7 +236,8 @@ class SECPE(object):
             #     syn_data.data_frame.reset_index(drop=True, inplace=True)
             #     syn_data.metadata.iteration = 0
             #     self._log_metrics(syn_data)
-
+            
+            syn_data_list = []
             syn_data = OpenReview(root_dir="/content/drive/MyDrive/SecPE/train")
             syn_data = Data.concat(syn_data_list, metadata=self._mix_data.metadata)
             syn_data.data_frame.reset_index(drop=True, inplace=True)
