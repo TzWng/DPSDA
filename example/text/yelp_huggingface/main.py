5e41e17066da267ccd83e570c76d9e9f7d55dac2
TzWng <91708166+TzWng@users.noreply.github.com>
2025-09-23 10:57:41 -0400

    Update main.py

--- example/text/yelp_huggingface/main.py
+++ example/text/yelp_huggingface/main.py
@@ -43,65 +43,65 @@ pd.options.mode.copy_on_write = True
 
 
 if __name__ == "__main__":
-    # exp_folder = "/content/drive/MyDrive/SecPE"
-    # current_folder = os.path.dirname(os.path.abspath(__file__))
+    exp_folder = "/content/drive/MyDrive/SecPE"
+    current_folder = os.path.dirname(os.path.abspath(__file__))
 
-    # setup_logging(log_file=os.path.join(exp_folder, "log.txt"))
+    setup_logging(log_file=os.path.join(exp_folder, "log.txt"))
 
-    # data = Yelp(root_dir="/content/drive/MyDrive/SecPE/upload")
-    # secrets = ['phillie', 'btwn', 'shh', 'bespoke', 'clot', 'allegria', 'rava', 'repost', 'gracing', 'gaminess', 'sri', 'plateau', 'perms', 'bobas', 'wince', 'virago', 'pana', '15k', 'sunglass', 'translating', 'banal', 'lupe', 'borgata', 'irreplaceable', 'conceptually', 'raided', 'purge', 'toki', 'wollensky', 'encyclopedic', 'surpassing', 'sharpen', 'bayalage', 'sneakily', '204', 'crunchwrap', 'sprinted', 'mythos', 'compromises', 'backdrops', 'dalton', 'umpteenth', 'bei', 'unknowing', 'eves', '2hours', 'gigante', 'marino', 'paccheri', 'micha', 'filomena', 'uploading', 'raffles', 'vikings', 'lindenwood', 'temperamental', 'murphys', 'slainte', 'undeserving', 'olfactory', 'yona', 'orthotics', 'sprinklers', '168', 'untrustworthy', 'fudgy', 'reconfirm', 'verduras', 'depict', 'plesant', 'carmelitas', 'cortadito', 'erie', 'yeesh', 'dipa', 'grump', 'woodworking', '2sp', 'fritatta', 'modem', 'narnia', 'gameday', 'diablitos', 'citadel', 'fc', 'snickered', 'caf', 'tapestry', 'agnes', 'anomalies', 'tolerating', 'cobra', 'gulping', 'outages', 'nobles', 'presenter', 'flopping', 'cupid', 'tabbouli', 'gateau', 'cocoon', 'marries', 'commonwealth', 'gobbling', 'folsom', 'healthiness', 'liquored', 'sniffed', 'shoves', 'résistance', 'garret', 'greene', 'fasciitis', 'northerners', 'characterize', 'abysmally', 'probability', 'distressing', 'smarmy', 'whistling', 'jude', 'enterprises', 'coaxing', 'poser', 'brookie', 'evils', 'discusses', 'calabria', 'waaayyy', 'chasse', 'automobiles', 'instructs', 'bandaids', 'dabs', 'cystic', 'surcharges', 'guthrie', 'wring', 'tills', 'khing', 'ticketmaster', 'dorsett', 'anthonys', 'pyt', 'morally', 'ravaged', 'hte', 'dossant', 'radiohead', 'nuthin', 'crispies', 'tumbling', 'innings', 'intangibles', 'reinforces', 'runnier', 'amazinggggg', 'unconscionable', 'kohlrabi', 'playgrounds', 'fiddlehead', 'quetzally', 'etoile', 'vetted', 'schemes', 'roving', 'cheung', '133', 'hammonton', 'grasping', 'mailers', 'slumped', 'miserly', 'woodlands', 'cemetary', 'lardo', 'cite', 'pivotal', 'était', 'hiro', 'rockfish', 'wonderfull', 'hushpuppy', 'scalded', 'winspear', 'wearable', 'chihuly', 'coaxed', 'pouting', '4hrs', 'horde', 'niçoise', 'capitalist', 'buuuut', 'babylon', 'quotations', 'kickboxing', 'bullion', 'hott', 'dissect', 'bitchin', 'tequenos', 'marti', 'indulgences', 'emotionless', 'chelsey', 'camouflage', 'soggier', 'dodgers', 'teaming', 'coronary', 'fwot', 'admonished', 'crappier', 'isc', 'swimwear', 'singularly', 'totalled', 'marisco', 'inquisitive', 'carissa', 'lampshade', 'tallow', 'segunda', 'retaliation', 'updos', 'handprints', '445', 'johanna', 'pesticides', 'badgering', 'instinctively', 'dekes', 'reigning', 'jeyuk', 'throng', 'krēm', 'miner', 'interracial', 'qualifications', 'curators', 'reproduction', 'chemex', 'chex', 'foodservice', 'mazes', 'polaris', 'wrigley', 'asthmatic', 'downturn', 'brunchy', 'culinarily', 'membrane', 'hopsmith', 'bitched', 'awash', 'jennie', 'spawn', 'hammers', 'veterinarian', 'rotator', 'acceptably', '422', 'epi', 'boswell', 'unfocused', 'mouthy', 'quatro', 'psychologist', 'rani', 'burton', 'unbothered', 'kingfisher', 'eladio', 'expletive', 'bozo', 'laces', 'curacao', 'expereince', 'opp', 'omelete', 'malcolm', 'amour', 'volvo', 'freckles', 'crosswalk', 'uph', 'ofc', 'aunties', 'egress', 'upsale', '134', 'zodiac', 'mimics', '35min', 'preventive', 'bonita']
-    # llm = HuggingfaceLLM(max_completion_tokens=64, model_name_or_path="gpt2", temperature=1.4)
-    # api = LLMAugPE(
-    #     llm=llm,
-    #     random_api_prompt_file=os.path.join(current_folder, "random_api_prompt.json"),
-    #     variation_api_prompt_file=os.path.join(current_folder, "variation_api_prompt.json"),
-    # )
-    # embedding = SentenceTransformer(model="stsb-roberta-base-v2")
-    # histogram = NearestNeighbors(
-    #     embedding=embedding,
-    #     mode="cos_sim",
-    #     lookahead_degree=0,
-    #     num_clusters=600,
-    # )
-    # population = PEPopulation(
-    #     api=api, initial_variation_api_fold=6, next_variation_api_fold=6, keep_selected=True, selection_mode="rank"
-    # )
+    data = Yelp(root_dir="/content/drive/MyDrive/SecPE/upload")
+    secrets = ['phillie', 'btwn', 'shh', 'bespoke', 'clot', 'allegria', 'rava', 'repost', 'gracing', 'gaminess', 'sri', 'plateau', 'perms', 'bobas', 'wince', 'virago', 'pana', '15k', 'sunglass', 'translating', 'banal', 'lupe', 'borgata', 'irreplaceable', 'conceptually', 'raided', 'purge', 'toki', 'wollensky', 'encyclopedic', 'surpassing', 'sharpen', 'bayalage', 'sneakily', '204', 'crunchwrap', 'sprinted', 'mythos', 'compromises', 'backdrops', 'dalton', 'umpteenth', 'bei', 'unknowing', 'eves', '2hours', 'gigante', 'marino', 'paccheri', 'micha', 'filomena', 'uploading', 'raffles', 'vikings', 'lindenwood', 'temperamental', 'murphys', 'slainte', 'undeserving', 'olfactory', 'yona', 'orthotics', 'sprinklers', '168', 'untrustworthy', 'fudgy', 'reconfirm', 'verduras', 'depict', 'plesant', 'carmelitas', 'cortadito', 'erie', 'yeesh', 'dipa', 'grump', 'woodworking', '2sp', 'fritatta', 'modem', 'narnia', 'gameday', 'diablitos', 'citadel', 'fc', 'snickered', 'caf', 'tapestry', 'agnes', 'anomalies', 'tolerating', 'cobra', 'gulping', 'outages', 'nobles', 'presenter', 'flopping', 'cupid', 'tabbouli', 'gateau', 'cocoon', 'marries', 'commonwealth', 'gobbling', 'folsom', 'healthiness', 'liquored', 'sniffed', 'shoves', 'résistance', 'garret', 'greene', 'fasciitis', 'northerners', 'characterize', 'abysmally', 'probability', 'distressing', 'smarmy', 'whistling', 'jude', 'enterprises', 'coaxing', 'poser', 'brookie', 'evils', 'discusses', 'calabria', 'waaayyy', 'chasse', 'automobiles', 'instructs', 'bandaids', 'dabs', 'cystic', 'surcharges', 'guthrie', 'wring', 'tills', 'khing', 'ticketmaster', 'dorsett', 'anthonys', 'pyt', 'morally', 'ravaged', 'hte', 'dossant', 'radiohead', 'nuthin', 'crispies', 'tumbling', 'innings', 'intangibles', 'reinforces', 'runnier', 'amazinggggg', 'unconscionable', 'kohlrabi', 'playgrounds', 'fiddlehead', 'quetzally', 'etoile', 'vetted', 'schemes', 'roving', 'cheung', '133', 'hammonton', 'grasping', 'mailers', 'slumped', 'miserly', 'woodlands', 'cemetary', 'lardo', 'cite', 'pivotal', 'était', 'hiro', 'rockfish', 'wonderfull', 'hushpuppy', 'scalded', 'winspear', 'wearable', 'chihuly', 'coaxed', 'pouting', '4hrs', 'horde', 'niçoise', 'capitalist', 'buuuut', 'babylon', 'quotations', 'kickboxing', 'bullion', 'hott', 'dissect', 'bitchin', 'tequenos', 'marti', 'indulgences', 'emotionless', 'chelsey', 'camouflage', 'soggier', 'dodgers', 'teaming', 'coronary', 'fwot', 'admonished', 'crappier', 'isc', 'swimwear', 'singularly', 'totalled', 'marisco', 'inquisitive', 'carissa', 'lampshade', 'tallow', 'segunda', 'retaliation', 'updos', 'handprints', '445', 'johanna', 'pesticides', 'badgering', 'instinctively', 'dekes', 'reigning', 'jeyuk', 'throng', 'krēm', 'miner', 'interracial', 'qualifications', 'curators', 'reproduction', 'chemex', 'chex', 'foodservice', 'mazes', 'polaris', 'wrigley', 'asthmatic', 'downturn', 'brunchy', 'culinarily', 'membrane', 'hopsmith', 'bitched', 'awash', 'jennie', 'spawn', 'hammers', 'veterinarian', 'rotator', 'acceptably', '422', 'epi', 'boswell', 'unfocused', 'mouthy', 'quatro', 'psychologist', 'rani', 'burton', 'unbothered', 'kingfisher', 'eladio', 'expletive', 'bozo', 'laces', 'curacao', 'expereince', 'opp', 'omelete', 'malcolm', 'amour', 'volvo', 'freckles', 'crosswalk', 'uph', 'ofc', 'aunties', 'egress', 'upsale', '134', 'zodiac', 'mimics', '35min', 'preventive', 'bonita']
+    llm = HuggingfaceLLM(max_completion_tokens=64, model_name_or_path="gpt2", temperature=1.4)
+    api = LLMAugPE(
+        llm=llm,
+        random_api_prompt_file=os.path.join(current_folder, "random_api_prompt.json"),
+        variation_api_prompt_file=os.path.join(current_folder, "variation_api_prompt.json"),
+    )
+    embedding = SentenceTransformer(model="stsb-roberta-base-v2")
+    histogram = NearestNeighbors(
+        embedding=embedding,
+        mode="cos_sim",
+        lookahead_degree=0,
+        num_clusters=600,
+    )
+    population = PEPopulation(
+        api=api, initial_variation_api_fold=6, next_variation_api_fold=6, keep_selected=True, selection_mode="rank"
+    )
 
-    # save_checkpoints = SaveCheckpoints(os.path.join(exp_folder, "checkpoint"))
-    # # compute_fid = ComputeFID(
-    # #     priv_data=data, embedding=embedding, filter_criterion={VARIATION_API_FOLD_ID_COLUMN_NAME: -1}
-    # # )
-    # # with open("/content/drive/MyDrive/SecPE/compute_fid_yelp.pkl", "wb") as f:
-    # #     pickle.dump(compute_fid, f)
-    # with open("/content/drive/MyDrive/SecPE/compute_fid_yelp.pkl", "rb") as f:
-    #     compute_fid = pickle.load(f)
+    save_checkpoints = SaveCheckpoints(os.path.join(exp_folder, "checkpoint"))
+    # compute_fid = ComputeFID(
+    #     priv_data=data, embedding=embedding, filter_criterion={VARIATION_API_FOLD_ID_COLUMN_NAME: -1}
+    # )
+    # with open("/content/drive/MyDrive/SecPE/compute_fid_yelp.pkl", "wb") as f:
+    #     pickle.dump(compute_fid, f)
+    with open("/content/drive/MyDrive/SecPE/compute_fid_yelp.pkl", "rb") as f:
+        compute_fid = pickle.load(f)
         
-    # save_text_to_csv = SaveTextToCSV(output_folder=os.path.join(exp_folder, "synthetic_text"))
+    save_text_to_csv = SaveTextToCSV(output_folder=os.path.join(exp_folder, "synthetic_text"))
 
-    # csv_print = CSVPrint(output_folder=exp_folder)
-    # log_print = LogPrint()
+    csv_print = CSVPrint(output_folder=exp_folder)
+    log_print = LogPrint()
 
-    # num_private_samples = len(data.data_frame)
-    # delta = 1.0 / num_private_samples / np.log(num_private_samples)
+    num_private_samples = len(data.data_frame)
+    delta = 1.0 / num_private_samples / np.log(num_private_samples)
 
-    # pe_runner = SECPE(
-    #     mix_data=data,
-    #     embedding=embedding,
-    #     secrets=secrets,
-    #     population=population,
-    #     histogram=histogram,
-    #     callbacks=[save_checkpoints, save_text_to_csv, compute_fid],
-    #     loggers=[csv_print, log_print],
-    # )
-    # J = len(secrets)
-    # p = np.full(J, 1e-4, dtype=np.float64)
-    # r = 0 * p
-    # pe_runner.run(
-    #     num_samples_schedule=[5000] * 2,
-    #     p=p, r=r,
-    #     checkpoint_path=os.path.join(exp_folder, "checkpoint"),
-    # )
-    data = Data()
-    data.load_checkpoint("/content/drive/MyDrive/SecPE/pubmed_cluster4000_2p/checkpoint/000000005")
-    data = data.filter({VARIATION_API_FOLD_ID_COLUMN_NAME: -1})
-    SaveTextToCSV(output_folder="/content/drive/MyDrive/SecPE/pubmed_secpe/checkpoint/new")(data)
+    pe_runner = PE(
+        mix_data=data,
+        embedding=embedding,
+        secrets=secrets,
+        population=population,
+        histogram=histogram,
+        callbacks=[save_checkpoints, save_text_to_csv, compute_fid],
+        loggers=[csv_print, log_print],
+    )
+    J = len(secrets)
+    p = np.full(J, 1e-4, dtype=np.float64)
+    r = 2 * p
+    pe_runner.run(
+        num_samples_schedule=[5000] * 2,
+        p=p, r=r,
+        checkpoint_path=os.path.join(exp_folder, "checkpoint"),
+    )
+    # data = Data()
+    # data.load_checkpoint("/content/drive/MyDrive/SecPE/pubmed_cluster4000_2p/checkpoint/000000005")
+    # data = data.filter({VARIATION_API_FOLD_ID_COLUMN_NAME: -1})
+    # SaveTextToCSV(output_folder="/content/drive/MyDrive/SecPE/pubmed_secpe/checkpoint/new")(data)
