022774e6415a2e7c6ba98f21204f01406694a6de
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-07 21:13:53 -0500

    Update downstream_data.py

--- example/text/yelp_huggingface/downstream_data.py
+++ example/text/yelp_huggingface/downstream_data.py
@@ -2,7 +2,8 @@ from pe.data import Data
 from pe.callback import SaveTextToCSV
 from pe.constant.data import VARIATION_API_FOLD_ID_COLUMN_NAME
 
-data = Data()
-data.load_checkpoint("/content/drive/MyDrive/SecPE/yelp_pii_diff_secpe600_10_new/checkpoint/000000005")
-data = data.filter({VARIATION_API_FOLD_ID_COLUMN_NAME: -1})
-SaveTextToCSV(output_folder="/content/drive/MyDrive/SecPE/yelp_pii_diff_secpe600_10_new/")(data)
+for rp in [2, 10, 50]:
+    data = Data()
+    data.load_checkpoint(f"/content/drive/MyDrive/SecPE/yelp_pii_diff_secpe600_{rp}/checkpoint/000000005")
+    data = data.filter({VARIATION_API_FOLD_ID_COLUMN_NAME: -1})
+    SaveTextToCSV(output_folder=f"/content/drive/MyDrive/SecPE/yelp_pii_diff_secpe600_{rp}/")(data)
