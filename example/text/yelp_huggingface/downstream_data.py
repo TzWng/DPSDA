a79fbba653133091d51e89856969a542e697ab7e
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-15 22:26:27 -0500

    Refactor data loading and saving logic

--- example/text/yelp_huggingface/downstream_data.py
+++ example/text/yelp_huggingface/downstream_data.py
@@ -13,16 +13,16 @@ sys.modules.setdefault('numpy._core.overrides',  npcore.overrides)
 import pandas as pd  # 现在再导入 pandas
 
 
-for rp in [2, 10, 50]:
-    data = Data()
-    data.load_checkpoint(f"/content/drive/MyDrive/SecPE/Yelp_different_K/yelp_huggingface_gpt2_mugdp_{rp}_0_pii_5000_20/000000010")
-    data = data.filter({VARIATION_API_FOLD_ID_COLUMN_NAME: -1})
-    SaveTextToCSV(output_folder=f"/content/drive/MyDrive/SecPE/Yelp_different_K/yelp_huggingface_gpt2_mugdp_{rp}_0_pii_5000_20/")(data)
-
-# for k in [200, 1000, 1200, 1600, 1800]:
+# for rp in [2, 10, 50]:
 #     data = Data()
-#     data.load_checkpoint(f"/content/drive/MyDrive/SecPE/Yelp_different_K/yelp_huggingface_gpt2_secpe_10_{k}_random_5000_20/000000020")
+#     data.load_checkpoint(f"/content/drive/MyDrive/SecPE/Yelp_different_K/yelp_huggingface_gpt2_mugdp_{rp}_0_pii_5000_20/000000010")
 #     data = data.filter({VARIATION_API_FOLD_ID_COLUMN_NAME: -1})
-#     SaveTextToCSV(output_folder=f"/content/drive/MyDrive/SecPE/Yelp_different_K/yelp_huggingface_gpt2_secpe_10_{k}_random_5000_20/")(data)
+#     SaveTextToCSV(output_folder=f"/content/drive/MyDrive/SecPE/Yelp_different_K/yelp_huggingface_gpt2_mugdp_{rp}_0_pii_5000_20/")(data)
+
+for k in [50, 100, 800]:
+    data = Data()
+    data.load_checkpoint(f"/content/drive/MyDrive/SecPE/Yelp_different_K/yelp_huggingface_gpt2_secpe_10_{k}_random_5000_20/000000020")
+    data = data.filter({VARIATION_API_FOLD_ID_COLUMN_NAME: -1})
+    SaveTextToCSV(output_folder=f"/content/drive/MyDrive/SecPE/Yelp_different_K/yelp_huggingface_gpt2_secpe_10_{k}_random_5000_20/")(data)
 
 
